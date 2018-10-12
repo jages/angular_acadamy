05bed7d8bb6a0eaaf0558ba390df8ea76475a633
Jan Geske <16306035+jages@users.noreply.github.com>
2018-10-12 15:53:56 +0200

    chat messages can now be added with left and right click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  message: string;
+  messages: Message[];
+  leftUser: User;
+  rightUser: User;
 
+  constructor() {
+    this.message = '';
+    this.messages = [];
+    this.leftUser = new User('Lord Vader');
+    this.rightUser = new User('You');
+  }
+
+  clickButton() {
+    this.messages.push(new Message(this.message, 'right', this.rightUser.name));
+    this.message = '';
+  }
+
+  rightClickbutton() {
+    this.messages.push(new Message(this.message, 'left', this.leftUser.name));
+    this.message = '';
+
+    return false;
+  }
+}
+
+class Message {
+  constructor(message: string, alignment: string, user: string) {
+    this.text = message;
+    this.alignment = alignment;
+    this.from = user;
+  }
+  text: string;
+  alignment: string;
+  from: string;
+}
+
+class User {
+  constructor(name: string) {
+    this.name = name;
+  }
+  name: string;
 }
